38efc5cdc731cb115bc90ed9827a85ac64d26310
Roman Miroshnychenko <romanvm@yandex.ua>
2016-02-26 17:23:57 +0200

    Adds css view url

--- tinymce/urls.py
+++ tinymce/urls.py
@@ -1,9 +1,10 @@
 # coding: utf-8
 
-from tinymce.views import spell_check, filebrowser
+from tinymce.views import spell_check, filebrowser, css
 from django.conf.urls import url
 
 urlpatterns = [
     url(r'^spellchecker/$', spell_check, name='tinymce-spellchecker'),
     url(r'^filebrowser/$', filebrowser, name='tinymce-filebrowser'),
+    url(r'^css/$', css, name='tinymce-css'),
 ]

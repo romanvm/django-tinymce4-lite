7c97740b1475dc1043ad18c22f21970807111847
Roman Miroshnychenko <romanvm@yandex.ru>
2016-02-26 23:55:46 +0200

    Fixes tests for Django 1.7

--- test_project/settings.py
+++ test_project/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
-    'django.middleware.security.SecurityMiddleware',
+    # 'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

661c0a5a0161b2dfa389c536fbcc8db4a5c082ad
Roman Miroshnychenko <romanvm@yandex.ru>
2016-02-27 12:26:20 +0200

    Adds docstrings for package and settings

---
+++ tinymce/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# License: MIT, see LICENSE.txt
+"""
+django-tinymce4-lite
+
+This application provides rich-text WYSIWYG `TinyMCE 4`_ widget
+for Django forms and models.
+
+.. _TinyMCE 4: https://www.tinymce.com/
+"""
--- tinymce/settings.py
+++ tinymce/settings.py
@@ -1,10 +1,11 @@
 # coding: utf-8
+# License: MIT, see LICENSE.txt
+"""The django-tinymce4-lite configuration options"""
 
 from __future__ import absolute_import
 from django.conf import settings
 from django.contrib.staticfiles.storage import staticfiles_storage
 
-# Default TinyMCE 4 layout
 DEFAULT = {
     'selector': 'textarea',
     'theme': 'modern',
@@ -16,13 +17,30 @@ DEFAULT = {
     'statusbar': True,
     'height': 360,
 }
-
-JS_URL = staticfiles_storage.url('tinymce/js/tinymce/tinymce.min.js')
-CSS_URL = getattr(settings, 'TINYMCE_CSS_URL', None)
-CALLBACKS = getattr(settings, 'TINYMCE_CALLBACKS', {})
+"""Default TinyMCE 4 configuration"""
 USE_SPELLCHECKER = getattr(settings, 'TINYMCE_SPELLCHECKER', False)
-USE_FILEBROWSER = getattr(settings, 'TINYMCE_FILEBROWSER', False)
-CONFIG = getattr(settings, 'TINYMCE_DEFAULT_CONFIG', DEFAULT)
+"""Use tinymce4 built-in spellchecker service"""
 if USE_SPELLCHECKER:
     DEFAULT['plugins'] += ' spellchecker'
     DEFAULT['toolbar1'] += ' | spellchecker'
+CONFIG = getattr(settings, 'TINYMCE_DEFAULT_CONFIG', DEFAULT)
+"""TinyMCE 4 configuration"""
+JS_URL = getattr(settings, 'TINYMCE_JS_URL', staticfiles_storage.url('tinymce/js/tinymce/tinymce.min.js'))
+"""TinyMCE 4 JavaScript code"""
+CSS_URL = getattr(settings, 'TINYMCE_CSS_URL', None)
+"""
+Additional CSS styles for TinyMCE 4
+
+The default CSS is used to fix TinyMCE 4 position in Django Admin.
+"""
+CALLBACKS = getattr(settings, 'TINYMCE_CALLBACKS', {})
+"""TinyMCE 4 calback JavaScript functions"""
+USE_FILEBROWSER = getattr(settings, 'TINYMCE_FILEBROWSER', False)
+"""
+Enable integration with django-filebrowser
+
+Both `django-filebrowser`_ and `django-filebrowser-no-grappelli`_ are supported.
+
+.. _django-filebrowser: https://github.com/sehmaschine/django-filebrowser
+.. _django-filebrowser-no-grappelli: https://github.com/smacker/django-filebrowser-no-grappelli
+"""

d7f5105c80f5fce449d9e9109b79598bf64f7442
Roman Miroshnychenko <romanvm@yandex.ru>
2016-06-01 10:08:47 +0300

    Version bump: 1.0.4

--- metadata.py
+++ metadata.py
@@ -1,4 +1,4 @@
 name = 'tinymce4-lite'
-authors = 'Joost Cassee, Aljosa Mohorovic Roman Mirochnychenko'
-version = '1.0.3'
+authors = 'Joost Cassee, Aljosa Mohorovic, Roman Mirochnychenko'
+version = '1.0.4'
 release = version

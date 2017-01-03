bc0ee523512184821762629af03beb8bcd871075
Roman Miroshnychenko <romanvm@yandex.ua>
2017-01-03 13:59:17 +0200

    Updates classifiers in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import os
 from codecs import open
 from setuptools import setup
@@ -36,11 +37,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Framework :: Django',
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',

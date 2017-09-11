97f6f5ff99512d940e4efe8513f7b850e93bfd73
Roman Miroshnychenko <romanvm@yandex.ru>
2017-09-11 10:34:05 +0300

    Refactor tinymce_init.js template for better code structure

--- tinymce/templates/tinymce/tinymce_init.js
+++ tinymce/templates/tinymce/tinymce_init.js
@@ -1,22 +1,54 @@
 (function($) {
-  function tinymce4_init(selector) {
-    var tinymce4_config = {
-      {% for key, value in callbacks.items %}
-        '{{ key }}': {{ value|safe }},
-      {% empty %}
-      {% endfor %}
-      {{ tinymce_config|safe }}
-    };
-    if (typeof selector != 'undefined') {
-      tinymce4_config['selector'] = selector;
-    }
-    tinymce.init(tinymce4_config);
-  } // End tinymce4_init
+  {% if is_admin_inline %}
+  $(function() {
+  {% endif %}
+    function tinymce4_init(selector) {
+      var tinymce4_config = {
+        {% for key, value in callbacks.items %}
+          '{{ key }}': {{ value|safe }},
+        {% endfor %}
+        {{ tinymce_config|safe }}
+      };
+      if (typeof selector != 'undefined') {
+        tinymce4_config['selector'] = selector;
+      }
+      tinymce.init(tinymce4_config);
+    } // End tinymce4_init
 {% if not is_admin_inline %}
-  tinymce4_init();
+    tinymce4_init();
 })();
 {% else %}
-  $(function() {
+    // Add TinyMCE 4 widgets to textaras inside a node
+    function add_editors(node) {
+      $(node).find('.tinymce4-editor').each(function(i, elem) {
+        if ($(elem).css('display') != 'none' && elem.id.indexOf('__prefix__') == -1) {
+          tinymce4_init(elem.tagName + '#' + elem.id);
+        }
+      });
+    }
+
+    // Remove TinyMCE 4 widgets from textareas inside a node
+    function remove_editors(node) {
+      $(node).find('.tinymce4-editor').each(function(i, elem) {
+        $(tinymce.EditorManager.editors).each(function(i, editor) {
+          if (editor.id == elem.id) {
+            editor.remove();
+          }
+        });
+      });
+    }
+
+    // Restore consistency of TinyMCE 4 widgets
+    function refresh_editors() {
+      $(tinymce.EditorManager.editors).each(function(i, editor) {
+        var elem = editor.getElement();
+        if (editor.id != elem.id) {
+          editor.remove();
+          tinymce4_init(elem.tagName + '#' + elem.id);
+        }
+      });
+    }
+
     var inline_group = $('div.inline-group');
 
     // Use MutationObserver to track adding or removing Django admin inline formsets
@@ -25,32 +57,16 @@
       $(mutations).each(function(i, mutation) {
         $(mutation.addedNodes).each(function(i, node) {
           // Add TinyMCE widgets to new textareas.
-          $(node).find('.tinymce4-editor').each(function(i, elem) {
-            if ($(elem).css('display') != 'none' && elem.id.indexOf('__prefix__') == -1) {
-              tinymce4_init(elem.tagName + '#' + elem.id);
-            }
-          });
+          add_editors(node);
         }); // End addedNodes
         $(mutation.removedNodes).each(function(i, node) {
           // Remove TinyMCE widgets from textareas inside removed nodes.
-          $(node).find('.tinymce4-editor').each(function(i, elem) {
-            $(tinymce.EditorManager.editors).each(function(i, editor) {
-              if (editor.id == elem.id) {
-                editor.remove();
-              }
-            });
-          });
-          // Refresh remaining TinyMCE editors to return them to consistent state
+          remove_editors(node);
+          // Refresh remaining TinyMCE widgets to return them to consistent state
           // After removing an inline formset, Django admin scripts
           // change IDs of remaining textareas,
-          // so textarea ID != TinyMCE instance ID attached to it.
-          $(tinymce.EditorManager.editors).each(function(i, editor) {
-            var elem = editor.getElement();
-            if (editor.id != elem.id) {
-              editor.remove();
-              tinymce4_init(elem.tagName + '#' + elem.id);
-            }
-          });
+          // so textarea ID != ID of the TinyMCE instance attached to it.
+          refresh_editors();
         }); // End removedNodes
       }); // End mutations
     }); // End MutationObserver

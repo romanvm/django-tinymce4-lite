39b7c6fb5dcc2d8306c1b8c87765eca1291b8f24
Roman Miroshnychenko <romanvm@yandex.ru>
2017-12-15 14:47:09 +0200

    Add 'display' view to display content entered in TinyMCE

--- test_tinymce/views.py
+++ test_tinymce/views.py
@@ -1,9 +1,15 @@
 from __future__ import absolute_import
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from .models import TestModel
 
 
 class TestCreateView(CreateView):
     template_name = 'test_tinymce/create.html'
     fields = ('content',)
+    model = TestModel
+
+
+class TestDisplayView(DetailView):
+    template_name = 'test_tinymce/display.html'
+    context_object_name = 'test_model'
     model = TestModel

aa0d15ae37e34e6e7d8b05c1fa27895baa3f8b35
Roman Miroshnychenko <roman1972@gmail.com>
2018-09-07 22:57:44 +0300

    Remove unused import

--- tinymce/settings.py
+++ tinymce/settings.py
@@ -5,7 +5,6 @@
 from __future__ import absolute_import
 import re
 import sys
-import warnings
 from django.conf import settings
 from django.contrib.staticfiles.storage import staticfiles_storage
 
